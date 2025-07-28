f8f2230142b4a852c7ead5c4b10b9bb92fc7b979
Raul Ciucalau <ciucalauraul@gmail.com>
2025-07-28 20:36:26 +0200

    feat: implement MovableObject class

--- js/game.js
+++ js/game.js
@@ -1,10 +1,14 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new Character();
+let enemies = [
+    new PufferFish(),
+    new JellyFish()
+]
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    character.src = '../img/1.Sharkie/1.IDLE/1.png';
-    ctx.drawImage(character, 150, 150, 100, 150);
+    console.log('My charcater is', character);
+    
 }
