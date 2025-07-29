33e4c0f7c4ee4e8311ba4b2c0dc4789b3f05f1b1
Raul Ciucalau <ciucalauraul@gmail.com>
2025-07-29 22:14:54 +0200

    feat: add position and dimensions to enemies classes

--- models/enemies/jelly-fish.class.js
+++ models/enemies/jelly-fish.class.js
@@ -3,5 +3,10 @@ class JellyFish extends MovableObject {
 
     constructor() {
         super().loadImage('img/2.Enemy/2 Jelly fish/Regular damage/Lila 1.png');
+
+        this.x = 200 + Math.random() * 500;
+        this.y = 300;
+        this.height = 100;
+        this.width = 100;
     }
 }
