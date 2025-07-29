d21ba3937d445a333dfc826193369804a8f27ab6
Raul Ciucalau <ciucalauraul@gmail.com>
2025-07-29 21:30:39 +0200

    feat: update game structure by adding World class functionality and refining enemy classes

--- models/enemies/final-enemy.class.js
+++ models/enemies/final-enemy.class.js
@@ -1,3 +1,7 @@
 class FinalEnemy extends MovableObject {
     
+
+    constructor() {
+        super().loadImage('img/2.Enemy/3 Final Enemy/2.floating/1.png');
+    }
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,7 +1,14 @@
 class MovableObject {
-    x;
-    y;
+    x= 120;
+    y= 220;
     img;
+    height = 150;
+    width = 150;
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
 
 
     moveRight() {
