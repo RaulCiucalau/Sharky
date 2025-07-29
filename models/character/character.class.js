4924bd43f0d94eec96778c59b94af99fb5412fc9
Raul Ciucalau <ciucalauraul@gmail.com>
2025-07-29 22:15:12 +0200

    feat: add Character and Light classes with initial properties

--- models/character/character.class.js
+++ models/character/character.class.js
@@ -3,5 +3,9 @@ class Character extends MovableObject {
 
     constructor() {
         super().loadImage('img/1.Sharkie/1.IDLE/1.png');
+        this.x = 20;
+        this.y = 200;
+        this.height = 220;
+        this.width = 220;
     }
 }
---
+++ models/light/light.class.js
@@ -0,0 +1,9 @@
+class Light extends MovableObject {
+    constructor() {
+        super().loadImage('img/3. Background/Layers/1. Light/1.png');
+        this.x = 200;
+        this.y = 0;
+        this.height = 450;
+        this.width = 700;
+    }
+}
