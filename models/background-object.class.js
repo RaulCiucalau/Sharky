20a37d53f29e31c4de3c21d9d75ae5a35f5495f9
Raul Ciucalau <ciucalauraul@gmail.com>
2025-07-31 20:26:20 +0200

    feat: remove unused light, floor, water, and fondo scripts from index.html; update BackgroundObject constructor parameters

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,7 +1,10 @@
 class BackgroundObject extends MovableObject {
-    
+    width = 720;
+    height = 480;
 
-    constructor(imagePath, x, y) {
+    constructor(imagePath, x, height, width) {
         super().loadImage(imagePath);
+        this.x = x;
+        this.y = 480 - this.height;
     }
 }
