9fbbdf5027b8507d2417af4492a21ca095a4a7ae
Raul Ciucalau <ciucalauraul@gmail.com>
2025-07-31 20:35:26 +0200

    feat: update enemy positions and remove unused fondo and water classes; enhance World class with new background objects

--- models/enemies/jelly-fish.class.js
+++ models/enemies/jelly-fish.class.js
@@ -4,7 +4,7 @@ class JellyFish extends MovableObject {
     constructor() {
         super().loadImage('img/2.Enemy/2 Jelly fish/Regular damage/Lila 1.png');
 
-        this.x = 200 + Math.random() * 500;
+        this.x = 300 + Math.random() * 500;
         this.y = 300;
         this.height = 100;
         this.width = 100;
--- models/world.class.js
+++ models/world.class.js
@@ -6,9 +6,11 @@ class World {
 
     ]
     backgroundObjects = [
+        new BackgroundObject('img/3. Background/Layers/5. Water/D1.png', 0, 480, 720),
+        new BackgroundObject('img/3. Background/Layers/4.Fondo 2/D1.png', 0, 400, 720),
+        new BackgroundObject('img/3. Background/Layers/3.Fondo 1/D1.png', 0, 400, 720),
         new BackgroundObject('img/3. Background/Layers/1. Light/1.png', 20, 450, 700),
         new BackgroundObject('img/3. Background/Layers/2. Floor/D1.png', 0, 400, 720),
-
     ]
     canvas
     ctx;
