e683b23874a36a11fcb3bf6fa04ff8fc71dd5659
Raul Ciucalau <ciucalauraul@gmail.com>
2025-08-02 16:52:07 +0200

    feat: integrate keyboard controls into game initialization and event handling

--- js/game.js
+++ js/game.js
@@ -1,7 +1,44 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init() {
     canvas = document.getElementById('canvas');
-    world = new World(canvas);
+    world = new World(canvas, keyboard);
 }
+
+window.addEventListener("keydown", (e) => {
+    if (e.code == "ArrowRight") {
+        keyboard.right = true;
+    }
+    if (e.code == "ArrowLeft") {
+        keyboard.left = true;
+    }
+    if (e.code == "ArrowUp") {
+        keyboard.up = true;
+    }
+    if (e.code == "ArrowDown") {
+        keyboard.down = true;
+    }
+    if (e.code == "Space") {
+        keyboard.space = true;
+    }
+})
+
+window.addEventListener("keyup", (e) => {
+    if (e.code == "ArrowRight") {
+        keyboard.right = false;
+    }
+    if (e.code == "ArrowLeft") {
+        keyboard.left = false;
+    }
+    if (e.code == "ArrowUp") {
+        keyboard.up = false;
+    }
+    if (e.code == "ArrowDown") {
+        keyboard.down = false;
+    }
+    if (e.code == "Space") {
+        keyboard.space = false;
+    }
+})
