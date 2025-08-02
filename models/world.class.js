6b33ed9c848b7afc49e330945ac47264cddbf87d
Raul Ciucalau <ciucalauraul@gmail.com>
2025-08-02 17:38:25 +0200

    refactor: rearrange draw method to improve object rendering order

--- models/world.class.js
+++ models/world.class.js
@@ -30,13 +30,9 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
         this.addObjectToMap(this.backgroundObjects);
-
-        this.addToMap(this.character);
-
         this.addObjectToMap(this.enemies);
-
+        this.addToMap(this.character);
         requestAnimationFrame(() => this.draw());
     }
 
@@ -45,6 +41,7 @@ class World {
             this.addToMap(object);
         });
     }
+    
     addToMap(MovableObject) {
         if (MovableObject.otherDirection) {
             this.flipImage(MovableObject);
