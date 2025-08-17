678212824a9b97bbc85755451db6882072fcad30
Raul Ciucalau <ciucalauraul@gmail.com>
2025-08-17 18:10:29 +0200

    add status bar coin

--- models/status-bar-class.js
+++ models/status-bar-class.js
@@ -1,5 +1,5 @@
 class StatusBar extends DrawableObject {
-    IMAGES = [
+    IMAGES_LIFE = [
         'img/4. Marcadores/green/Life/0_copia.png', // 0%
         'img/4. Marcadores/green/Life/20_copia.png', // 20%
         'img/4. Marcadores/green/Life/40_copia.png', // 40%
@@ -7,21 +7,39 @@ class StatusBar extends DrawableObject {
         'img/4. Marcadores/green/Life/80_copia.png', // 80%
         'img/4. Marcadores/green/Life/100_copia.png', // 100%
     ];
+    IMAGES_BOTTLES = [
+        'img/4. Marcadores/green/poisoned bubbles/0_ copia 2.png',
+        'img/4. Marcadores/green/poisoned bubbles/20_ copia 3.png',
+        'img/4. Marcadores/green/poisoned bubbles/40_ copia 2.png',
+        'img/4. Marcadores/green/poisoned bubbles/60_ copia 2.png',
+        'img/4. Marcadores/green/poisoned bubbles/80_ copia 2.png',
+        'img/4. Marcadores/green/poisoned bubbles/100_ copia 3.png',
+    ];
+    IMAGES_COINS = [
+        'img/4. Marcadores/green/Coin/0_copia 4.png',
+        'img/4. Marcadores/green/Coin/20_copia 2.png',
+        'img/4. Marcadores/green/Coin/40_copia 4.png',
+        'img/4. Marcadores/green/Coin/60_copia 4.png',
+        'img/4. Marcadores/green/Coin/80_copia 4.png',
+        'img/4. Marcadores/green/Coin/100_copia 4.png',
+    ];
     percentage = 100;
 
     constructor() {
         super();
-        this.loadImages(this.IMAGES);
+        this.loadImages(this.IMAGES_LIFE);
+        this.loadImages(this.IMAGES_BOTTLES);
+        this.loadImages(this.IMAGES_COINS);
         this.width = 220;
         this.height = 60;
         this.x = 0;
-        this.y = 0;
+        this.y = 30;
         this.setPercentage(100);
     }
 
     setPercentage(percentage) {
         this.percentage = percentage;
-        let path = this.IMAGES[this.resolveImageIndex(percentage)];
+        let path = this.IMAGES_LIFE[this.resolveImageIndex(percentage)];
         this.img = this.imageCache[path];
     }
 
@@ -32,5 +50,19 @@ class StatusBar extends DrawableObject {
         if (percentage >= 40) return 2;
         if (percentage >= 20) return 1;
         return 0;
+    }
+
+    drawBottleBar(ctx, percentage, x = 0, y = 0) {
+        let index = this.resolveImageIndex(percentage);
+        let path = this.IMAGES_BOTTLES[0];
+        let img = this.imageCache[path];
+        ctx.drawImage(img, x, y, this.width, this.height);
+    }
+
+    drawCoinBar(ctx, percentage, x = 0, y = 0) {
+        let index = this.resolveImageIndex(percentage);
+        let path = this.IMAGES_COINS[0];
+        let img = this.imageCache[path];
+        ctx.drawImage(img, x, y, this.width, this.height);
     }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -6,6 +6,8 @@ class World {
     keyboard;
     camera_x = 0;
     statusBar = new StatusBar();
+    bottlesBar = new StatusBar();
+    coinsBar = new StatusBar();
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -33,16 +35,29 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.save();
         this.ctx.translate(this.camera_x, 0);
         this.addObjectToMap(this.level.backgroundObjects);
-        this.ctx.translate(-this.camera_x, 0);
-        this.addToMap(this.statusBar);
-        this.ctx.translate(this.camera_x, 0);
         this.addObjectToMap(this.level.enemies);
         this.addObjectToMap(this.level.coins);
         this.addObjectToMap(this.level.poison);
         this.addToMap(this.character);
-        this.ctx.translate(-this.camera_x, 0);
+        this.ctx.restore();
+        this.statusBar.x = 20;
+        this.statusBar.y = 0;
+        this.statusBar.draw(this.ctx);
+        this.bottlesBar.x = 240;
+        this.bottlesBar.y = 0;
+        let bottlePercent = this.character.bottlePercentage !== undefined ? this.character.bottlePercentage : 0;
+        let bottlePath = this.bottlesBar.IMAGES_BOTTLES[this.bottlesBar.resolveImageIndex(bottlePercent)];
+        this.bottlesBar.img = this.bottlesBar.imageCache[bottlePath];
+        this.bottlesBar.draw(this.ctx);
+        this.coinsBar.x = 470;
+        this.coinsBar.y = 0;
+        let coinPercent = this.character.coinPercentage !== undefined ? this.character.coinPercentage : 0;
+        let coinPath = this.coinsBar.IMAGES_COINS[this.coinsBar.resolveImageIndex(coinPercent)];
+        this.coinsBar.img = this.coinsBar.imageCache[coinPath];
+        this.coinsBar.draw(this.ctx);
         requestAnimationFrame(() => this.draw());
     }
 
